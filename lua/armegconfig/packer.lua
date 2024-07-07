b01e0b57c2b46d1e275f9065fbbe8cce0b7e06ee
8Oldr <8Oldr@protonmail.com>
2024-07-07 19:08:54 +0300

    Personal changes, and added vimtex for uni stuff

--- lua/armegconfig/packer.lua
+++ lua/armegconfig/packer.lua
@@ -74,5 +74,15 @@ return require('packer').startup(function(use)
   use("eandrju/cellular-automaton.nvim")
   use("laytan/cloak.nvim")
 
+  use {
+      "lervag/vimtex",
+      lazy = false,     -- we don't want to lazy load VimTeX
+      -- tag = "v2.15", -- uncomment to pin to a specific release
+      init = function()
+        -- VimTeX configuration goes here, e.g.
+        vim.g.vimtex_view_method = "zathura"
+      end
+    }
+
 end)
 
