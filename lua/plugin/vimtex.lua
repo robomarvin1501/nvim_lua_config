5666183df19bb65a797eb86ac18cc872934a21ad
8Oldr <8Oldr@protonmail.com>
2025-08-22 16:13:56 +0300

    Cleanup of old files from old configs

--- lua/plugin/vimtex.lua
+++
@@ -1,9 +0,0 @@
-vim.g.tex_flavor = 'latex'
-vim.g.vimtex_view_method = 'zathura'
-vim.g.vimtex_quickfix_mode = 0
-vim.o.conceallevel = 1
-vim.g.tex_conceal = 'abdmg'
-
-vim.keymap.set("n", "<leader>li", string.format(":!make FILENAME=%s<CR>", vim.api.nvim_buf_get_name(0)));
-vim.keymap.set("n", "<leader>lb", string.format(":!make build FILENAME=%s<CR>", vim.fn.expand("%:p")));
-vim.keymap.set("n", "<leader>lw", vim.api.nvim_buf_get_name(0));
